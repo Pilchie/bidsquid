731c278bbef7264aa5e2bc5dcd1d4615d66f387b
Kevin Pilch <me@pilchie.com>
2017-08-02 10:11:36 -0600

    Fix date

--- test/bidsquid-test/bidsquid-test/AuctionViewModelTests.cs
+++ test/bidsquid-test/bidsquid-test/AuctionViewModelTests.cs
@@ -10,7 +10,7 @@ namespace bidsquid_test
         public void TestConstruction()
         {
             const string tagLine = "Fun times at KCDC";
-            var auction = new AuctionViewModel(new DateTime(2017, 08, 01), tagLine);
+            var auction = new AuctionViewModel(new DateTime(2017, 08, 02), tagLine);
             Assert.Equal(DateTime.Today, auction.Date);
             Assert.Equal(tagLine, auction.TagLine);
         }
